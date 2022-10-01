57f8a69fb7c0f7e734cf855e4f788432db5b79ee
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2022-09-30 18:40:34 -0700

    Create Subset_PIDA.R

---
+++ Subset_PIDA.R
@@ -0,0 +1,82 @@
+### PIDA ###
+pida <- read.csv(file.choose(),header=TRUE)
+
+### Load eLSA Output and Filter ###
+# Load data
+surf <- read.delim(file.choose(),header=TRUE)
+dcm <- read.delim(file.choose(),header=TRUE)
+
+# Subset based on p and q-value
+surfSig <- subset(surf,Psscc<0.01 & Qsscc < 0.01)
+dcmSig <- subset(dcm,Psscc<0.01 & Qsscc < 0.01)
+
+# Subset lagged and unlagged surface
+surfLag <- subset(surfSig,Delay!=0)
+surfNoLag <- subset(surfSig,Delay==0)
+surfLagG <- data.frame(x=c(surfLag$X),y=c(surfLag$Y),scc=c(surfLag$SCC),delay=c(surfLag$Delay))
+surfNoLagG <- data.frame(x=c(surfNoLag$X),y=c(surfNoLag$Y),scc=c(surfNoLag$SCC),delay=c(surfNoLag$Delay))
+
+### Plot lagged and unlagged networks surface ###
+# Unlagged surface
+g <- graph_from_data_frame(surfNoLagG,directed=FALSE)
+# plot(g,vertex.size=4,vertex.label=NA,edge.width=0.1,layout=layout_with_lgl(g))
+
+# Lagged surface
+surfLagG$new <- ifelse(surfLagG$delay==-1,surfLagG$y,NA)
+surfLagG$y <- ifelse(surfLagG$delay==-1,surfLagG$x,surfLagG$y)
+surfLagG$x <- ifelse(surfLagG$delay==-1,surfLagG$new,surfLagG$x)
+surfLagG$new <- NULL
+
+gLag <- graph_from_data_frame(surfLagG, directed=TRUE)
+# plot(gLag,vertex.size=4,vertex.label=NA,edge.width=0.1,layout=layout_with_lgl(gLag),edge.arrow.size=0.2)
+
+# Subset lagged and unlagged DCM
+DCMLag <- subset(dcmSig,Delay!=0)
+DCMNoLag <- subset(dcmSig,Delay==0)
+DCMLagG <- data.frame(x=c(DCMLag$X),y=c(DCMLag$Y),scc=c(DCMLag$SCC),delay=c(DCMLag$Delay))
+DCMNoLagG <- data.frame(x=c(DCMNoLag$X),y=c(DCMNoLag$Y),scc=c(DCMNoLag$SCC),delay=c(DCMNoLag$Delay))
+
+### Plot lagged and unlagged networks DCM ###
+# Unlagged DCM
+g2 <- graph_from_data_frame(DCMNoLagG,directed=FALSE)
+# plot(g2,vertex.size=4,vertex.label=NA,edge.width=0.1,layout=layout_with_lgl(g2))
+
+# Lagged DCM
+DCMLagG$new <- ifelse(DCMLagG$delay==-1,DCMLagG$y,NA)
+DCMLagG$y <- ifelse(DCMLagG$delay==-1,DCMLagG$x,DCMLagG$y)
+DCMLagG$x <- ifelse(DCMLagG$delay==-1,DCMLagG$new,DCMLagG$x)
+DCMLagG$new <- NULL
+
+gLag2 <- graph_from_data_frame(DCMLagG, directed=TRUE)
+# plot(gLag2,vertex.size=4,vertex.label=NA,edge.width=0.1,layout=layout_with_lgl(gLag2),edge.arrow.size=0.2)
+
+### Find edges that are in surface and DCM networks ###
+tax <- read.csv(file.choose(),header=TRUE,row.names=1)
+int <- intersection(g,g2) # Only edges that are in both
+
+tax2 <- tax[c(3:13)]
+colnames(tax2)<- c("Kingdom","Supergroup","Divison","Class","Order","Family","Genus","Species","V1","Final","Low")
+
+intEdge <- as.data.frame(get.edgelist(int))
+intEdge <- left_join(intEdge,tax2)
+colnames(tax2)<- c("Kingdom2","Supergroup2","Divison2","Class2","Order2","Family2","Genus2","Species2","V2","Final2","Low2")
+intEdge <- left_join(intEdge,tax2)
+
+out <- NULL
+for (i in 1:nrow(intEdge)){
+  tmp <- intEdge[102, ]
+  if(tmp$Genus %in% pida$Genus.org1 | tmp$Genus %in% pida$Genus.org2 | tmp$Genus %in% pida$Species.org1|tmp$Genus %in% pida$Species.org2){
+    sub <- pida %>% filter_all(any_vars(str_detect(., pattern = paste(tmp$Genus))))
+    sub <- sub %>% filter_all(any_vars(str_detect(., pattern = paste(tmp$Genus2))))
+    if (nrow(sub) > 0){
+      out <- rbind(out,sub)
+    }}}
+
+
+try <- data.frame(f1=out$Taxonomic.level.3..org1,g1=out$Genus.org1,s1=out$Species.org1,f2=out$Taxonomic.level.3..org2, g2=out$Genus.org2,s2=out$Species.org2)
+ 
+try <- try %>% distinct(f1,g1,s1,f2,g2,s2)
+
+s <- intEdge %>% filter_all(any_vars(str_detect(., pattern = paste("Gyrodinium"))))
+
+s <- intEdge %>% filter(Genus=="Gymnodinium"|Genus2=="Gymnodinium")
