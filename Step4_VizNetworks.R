a530cfeeb77c092d161093400de7fc19c4a3ac4d
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-02-14 20:31:03 -0500

    Update Step4_VizNetworks.R

--- Step4_VizNetworks.R
+++ Step4_VizNetworks.R
@@ -1,7 +1,11 @@
 ### SPOT Network Analysis ###
 ### Visualize Networks + Calculate Network Statistics ###
 ### By: Samantha Gleich ###
-### Last Updated: 12/28/22 ###
+### Last Updated: 2/14/23 ###
+
+taxCols <- c("#E1746D","#76C3D7","#DE5AB1","#D5E0AF","#DED3DC","#87EB58","#D4DC60","#88E5D3","#88AAE1","#DBA85C","#8B7DDA","#9A8D87","#D99CD1","#B649E3","#7EDD90")
+names(taxCols) <- c("Chlorophyte","Ciliate","Cryptophyte","Diatom","Dinoflagellate","Fungi","Haptophyte","MAST","Other Alveolate","Other Archaeplastida","Other Eukaryote","Other Stramenopile","Rhizaria","Syndiniales","Unknown Eukaryote")
+
 
 # Load in network outputs
 surf <- read.csv(file.choose(),header=TRUE,row.names=1)
@@ -83,3 +87,60 @@ dcmEdgeOnly <- as.data.frame(dcmEdgeOnly)
 write.csv(interEdge,"Surf_DCM_edges.csv")
 write.csv(surfEdgeOnly,"Surf_edges.csv")
 write.csv(dcmEdgeOnly,"DCM_edges.csv")
+
+
+# Size by degree
+dfDeg <- data.frame(name=c(V(gInterOnly)$name),deg=c(V(gInterOnly)$deg))
+dfDeg <- dfDeg %>% arrange(desc(deg)) %>% as.data.frame()
+dfDeg$name <- str_replace_all(dfDeg$name,"S_","")
+t <- read.delim("taxonomy_90.tsv",header=TRUE)
+t$Confidence <- NULL
+colnames(t)[1] <- "name"
+dfDeg <- left_join(dfDeg,t)
+
+# Color by tax
+taxz <- colsplit(dfDeg$Taxon,";",c("Supergroup","Kingdom","Phylum","Class","Order","Family","Genus","Species"))
+taxz$fin <- ifelse(taxz$Class=="Syndiniales","Syndiniales",NA)
+taxz$fin <- ifelse(taxz$Class=="Dinophyceae","Dinoflagellate",taxz$fin)
+taxz$fin <- ifelse(taxz$Class=="Bacillariophyta","Diatom",taxz$fin)
+taxz$fin <- ifelse(grepl("MAST",taxz$Class),"MAST",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Rhizaria","Rhizaria",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Chlorophyta","Chlorophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Cryptophyta","Cryptophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Haptophyta","Haptophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Ciliophora","Ciliate",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Stramenopiles" & is.na(taxz$fin),"Other Stramenopiles",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Archaeplastida" & is.na(taxz$fin),"Other Archaeplastids",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Alveolata" & is.na(taxz$fin),"Other Alveolate",taxz$fin)
+taxz$fin <- ifelse(is.na(taxz$fin),"Other Eukaryote",taxz$fin)
+unique(taxz$fin)
+
+dfDeg$tax <- taxz$fin
+V(gInterOnly)$tax <- dfDeg$tax
+taxColsDf <- data.frame(taxCols)
+taxColsDf$tax <- rownames(taxColsDf)
+dfDeg <- left_join(dfDeg,taxColsDf)
+V(gInterOnly)$col <- dfDeg$taxCols
+
+pdf("../../Degree.pdf")
+plot(gInterOnly,vertex.size=V(gInterOnly)$deg*0.5,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gInterOnly),vertex.color=V(gInterOnly)$col,edge.color="grey30",main="")
+dev.off()
+
+counts <- read.delim("feature-table.tsv",header=TRUE,row.names=1)
+counts <- as.data.frame(t(counts))
+counts <- counts/rowSums(counts)
+counts <- as.data.frame(t(counts))
+counts <- subset(counts,rownames(counts) %in% dfDeg$name)
+counts$meanz <- rowMeans(counts)
+counts$median = apply(counts, 1, median, na.rm=T)
+c <- data.frame(name=c(rownames(counts)),meanz=c(counts$meanz))
+#c <- data.frame(name=c(rownames(counts)),median=c(counts$median))
+dfDeg <- left_join(dfDeg,c)
+V(gInterOnly)$ab2 <- dfDeg$meanz
+
+pdf("../../Abundance3.pdf")
+plot(gInterOnly,vertex.size=V(gInterOnly)$ab2*800,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gInterOnly),vertex.color=V(gInterOnly)$col,edge.color="grey30",main="")
+dev.off()
+
+ggplot(dfDeg,aes(x=deg,y=meanz,fill=tax))+geom_point(shape=21,size=2)+scale_fill_manual(name=c("Taxonomic Group"),values=c(taxCols))+theme_classic(base_size = 14)+xlab("Number of Associations")+ylab("Relative Abundance")+ylim(0,0.06)+theme(legend.position='top')
+ggsave("../../plot2.pdf",width=8,height=4)
