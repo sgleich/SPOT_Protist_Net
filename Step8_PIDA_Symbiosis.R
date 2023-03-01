693dca500b75db1feec5e2aa5c08b90c3868d864
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-03-01 14:45:36 -0500

    Update Step8_PIDA_Symbiosis.R

--- Step8_PIDA_Symbiosis.R
+++ Step8_PIDA_Symbiosis.R
@@ -1,22 +1,54 @@
 ### SPOT Network Analysis ###
 ### PIDA Comparison ###
 ### By: Samantha Gleich ###
-### Last Updated: 1/3/23 ###
+### Last Updated: 3/1/23 ###
 
 # Colors to use for all taxonomic analyses
 taxCols <- c("#E1746D","#76C3D7","#DE5AB1","#D5E0AF","#DED3DC","#87EB58","#D4DC60","#88E5D3","#88AAE1","#DBA85C","#8B7DDA","#9A8D87","#D99CD1","#B649E3","#7EDD90")
 names(taxCols) <- c("Chlorophyte","Ciliate","Cryptophyte","Diatom","Dinoflagellate","Fungi","Haptophyte","MAST","Other Alveolate","Other Archaeplastida","Other Eukaryote","Other Stramenopile","Rhizaria","Syndiniales","Unknown Eukaryote")
 
-interEdge <- read.csv("Surf_DCM_Edges.csv",header=TRUE,row.names=1)
-interEdge$V1 <- str_remove_all(interEdge$V1,"S_")
-interEdge$V2 <- str_remove_all(interEdge$V2,"S_")
+s5 <- read.csv(file.choose(),header=TRUE,row.names=1)
+sDCM <- read.csv(file.choose(),header=TRUE,row.names=1)
+colnames(s5) <- str_remove_all(colnames(s5),"^X")
+colnames(sDCM) <- str_remove_all(colnames(sDCM),"^X")
+
+s5 <- as.matrix(s5)
+sDCM <- as.matrix(sDCM)
+
+s5[s5<0] <- -1
+s5[s5>0] <- 1
+sDCM[sDCM<0] <- -1
+sDCM[sDCM>0] <- 1
+
+
+g5m <- graph_from_adjacency_matrix(s5,mode="undirected",weighted=TRUE)
+gdcm <- graph_from_adjacency_matrix(sDCM,mode="undirected",weighted=TRUE)
+
+int <- graph.intersection(g5m,gdcm)
+
+interEdge <- as.data.frame(get.edgelist(int))
+g5mEdge <- as.data.frame(get.edgelist(g5m))
+g5mEdge$weight5 <- E(g5m)$weight
+gdcmEdge <- as.data.frame(get.edgelist(gdcm))
+gdcmEdge$weightDCM <- E(gdcm)$weight
+
+interEdge$fin1 <- paste(interEdge$V1,interEdge$V2,sep="_")
+g5mEdge$fin1 <- paste(g5mEdge$V1,g5mEdge$V2,sep="_")
+g5mEdge$fin2 <- paste(g5mEdge$V2,g5mEdge$V1,sep="_")
+gdcmEdge$fin1 <- paste(gdcmEdge$V1,gdcmEdge$V2,sep="_")
+gdcmEdge$fin2 <- paste(gdcmEdge$V2,gdcmEdge$V1,sep="_")
+
+interEdge <- left_join(interEdge,g5mEdge)
+interEdge <- left_join(interEdge,gdcmEdge)
+
+interEdge <- subset(interEdge,weight5==weightDCM)
+
 tax <- read.delim("taxonomy_90.tsv",header=TRUE,row.names=NULL)
 tax$Confidence <- NULL
 colnames(tax) <- c("V1","Tax1")
 interEdge <- left_join(interEdge,tax)
 colnames(tax) <- c("V2","Tax2")
 interEdge <- left_join(interEdge,tax)
-write.csv(interEdge,"../PIDA_Prelim.csv")
 
 pida <- read.csv(file.choose(),header=TRUE) 
 pida <- subset(pida,Taxonomic.interaction=="Prot - Prot")
@@ -45,10 +77,10 @@ outSymb<- rbind(otherEdges,acanthEdges1,acanthEdges2)
 
 outSymb$Tax1 <- NULL
 outSymb$Tax2 <- NULL
-outSymb <- as.matrix(outSymb)
 
 # Plot 
 outG <-graph_from_data_frame(outSymb,directed=FALSE)
+E(outG)$weight <- outSymb$weight5
 outDf <- data.frame(name=c(V(outG)$name))
 colnames(outDf) <- "V2"
 outDf <- left_join(outDf,tax)
@@ -69,101 +101,40 @@ taxz$fin <- ifelse(taxz$Kingdom=="Archaeplastida" & is.na(taxz$fin),"Other Archa
 taxz$fin <- ifelse(taxz$Kingdom=="Alveolata" & is.na(taxz$fin),"Other Alveolate",taxz$fin)
 taxz$fin <- ifelse(is.na(taxz$fin),"Other Eukaryote",taxz$fin)
 
-outDf$Final2 <- taxz$fin
-taxz$Low <- paste(taxz$Genus,taxz$Species,sep="_")
-taxz$Low <- ifelse(taxz$Class=="Syndiniales",taxz$Order,taxz$Low)
-outDf$Low <- taxz$Low
+taxz <- colsplit(outDf$Tax2,";",c("Supergroup","Kingdom","Phylum","Class","Order","Family","Genus","Species"))
+taxz$fin <- ifelse(taxz$Class=="Syndiniales","Syndiniales",NA)
+taxz$fin <- ifelse(taxz$Class=="Dinophyceae","Dinoflagellate",taxz$fin)
+taxz$fin <- ifelse(taxz$Class=="Bacillariophyta","Diatom",taxz$fin)
+taxz$fin <- ifelse(grepl("MAST",taxz$Class),"MAST",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Rhizaria","Rhizaria",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Chlorophyta","Chlorophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Cryptophyta","Cryptophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Haptophyta","Haptophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Ciliophora","Ciliate",taxz$fin)
+taxz$fin <- ifelse(taxz$Phylum=="Metazoa","Metazoa",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Stramenopiles" & is.na(taxz$fin),"Other Stramenopiles",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Archaeplastida" & is.na(taxz$fin),"Other Archaeplastids",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Alveolata" & is.na(taxz$fin),"Other Alveolate",taxz$fin)
+taxz$fin <- ifelse(is.na(taxz$fin),"Other Eukaryote",taxz$fin)
 
-outDfNames <- data.frame(outDfNames=c(unique(outDf$Low)),num=c(1))
-for (i in 1:nrow(outDf)){
-  m <- outDf[i,4]
-  num <- subset(outDfNames,outDfNames==m)
-  outDf$Low[i] <- paste(outDf$Low[i],num$num,sep="_")
-  row <- which(outDfNames==m)
-  outDfNames[row,2] <- outDfNames[row,2]+1
-}
+V(outG)$fin <- taxz$fin
+taxz$Low <- paste(taxz$Genus, taxz$Species,sep="_")
+low <- colsplit(taxz$Low,"_",c("keep","keep2","toss"))
+low$keep <- ifelse(low$keep=="","Acantharea Clade F",low$keep)
+V(outG)$namez <- low$keep
 
-outSum <- outDf %>% group_by(Final2) %>% tally()
-
-chlor <- subset(outDf,Final2=="Chlorophyte")
-cil <- subset(outDf,Final2=="Ciliate")
-cry <- subset(outDf,Final2=="Cryptophyte")
-dia <- subset(outDf,Final2=="Diatom")
-dino <- subset(outDf,Final2=="Dinoflagellate")
-hapto <- subset(outDf,Final2=="Haptophyte")
-mast <- subset(outDf,Final2=="MAST")
-oeuk <- subset(outDf,Final2=="Other Eukaryote")
-ostr <- subset(outDf,Final2=="Other Stramenopile")
-rhiz <- subset(outDf,Final2=="Rhizaria")
-syn <- subset(outDf,Final2=="Syndiniales")
-unk <- subset(outDf,Final2=="Unknown Eukaryote")
-
-v <- c(outSum$Final2,chlor$num,cil$num,cry$num,dia$num,dino$num,hapto$num,mast$num,oeuk$num,ostr$num,rhiz$num,syn$num,unk$num)
-
-# Hierarchy df
-hierarchy <- data.frame(from=outDf$Final2,to=outDf$Low)
-tmp <- data.frame(from=c(rep("Origin",4)),to=c("Diatom","Haptophyte","MAST","Rhizaria"))
-hierarchy <- rbind(tmp,hierarchy)
-
-# Vertices df
-vertices <- data.frame(name=outDf$Low,value=1,group=outDf$Final2)
-tmp2 <- data.frame(name=c("Origin","Diatom","Haptophyte","MAST","Rhizaria"),value=c(1,1,1,1,1),group=c(NA,"Origin","Origin","Origin","Origin"))
-vertices <- rbind(tmp2,vertices)
-
-# Mygraph
-out <- as.data.frame(outSymb)
-join <- data.frame(V1=c(outDf$V2),Low1=c(outDf$Low))
-out <- left_join(out,join)
-colnames(join) <- c("V2","Low2")
-out <- left_join(out,join)
-
-mygraph <- graph_from_data_frame( hierarchy, vertices=vertices)
-
-connect <- data.frame(from=c(out$Low1),to=c(out$Low2),value=c(1))
-from  <-  match( connect$from, vertices$name)
-to  <-  match( connect$to, vertices$name)
-
-#Let's add information concerning the label we are going to add: angle, horizontal adjustement and potential flip
-#calculate the ANGLE of the labels
-vertices$id <- NA
-myleaves <- which(is.na( match(vertices$name, hierarchy$from) ))
-nleaves <- length(myleaves)
-vertices$id[ myleaves ] <- seq(1:nleaves)
-vertices$angle <- 90 - 360 * vertices$id / nleaves
-
-# calculate the alignment of labels: right or left
-# If I am on the left part of the plot, my labels have currently an angle < -90
-vertices$hjust <- ifelse( vertices$angle < -90, 1, 0)
-
-# flip angle BY to make them readable
-vertices$angle <- ifelse(vertices$angle < -90, vertices$angle+180, vertices$angle)
-
-# calculate the alignment of labels: right or left
-# If I am on the left part of the plot, my labels have currently an angle < -90
-vertices$hjust <- ifelse( vertices$angle < -90, 1, 0)
-
-# flip angle BY to make them readable
-vertices$angle <- ifelse(vertices$angle < -90, vertices$angle+180, vertices$angle)
-
-mygraph <- igraph::graph_from_data_frame( hierarchy, vertices=vertices )
-
-# The connection object must refer to the ids of the leaves:
-from  <-  match( connect$from, vertices$name)
-to  <-  match( connect$to, vertices$name)
+pdf("../../try.pdf",width=20,height=11)
+ggraph(outG, layout = 'linear', circular = TRUE) + geom_edge_arc(color="dodgerblue",alpha=0.6,width=1) + 
+  geom_node_text(aes(label = namez, angle = node_angle(x, y)), hjust = -0.1,size=3) +
+  geom_node_point(shape = 21, size = 4, aes(fill = fin)) +
+  theme_graph() +
+  coord_fixed(xlim = c(-1.4, 1.4), ylim = c(-1.4, 1.4)) +scale_fill_manual(name="Taxonomic Groups",values=c(taxCols))
+dev.off()
 
 
-# Plot network
-# v <- randomcoloR::distinctColorPalette(15)
-
-ggraph(mygraph, layout = 'dendrogram', circular = TRUE) + 
-  geom_node_point(aes(filter = leaf, x = x*1.05, y=y*1.05)) +
-  geom_conn_bundle(data = get_con(from = from, to = to), alpha=0.5, colour="grey", width=0.7) +
-  #geom_node_text(aes(x = x*1.1, y=y*1.1, filter = leaf, label=name, angle = angle, hjust=hjust), size=1.5, alpha=1) +
-  theme_void() +
-  theme(
-    legend.position="none",
-    plot.margin=unit(c(0,0,0,0),"cm"),
-  ) +
-  expand_limits(x = c(-1.2, 1.2), y = c(-1.2, 1.2))+theme(legend.position = "right")+geom_node_point(aes(filter = leaf, x = x*1.05, y=y*1.05, colour=group),   size=2) +
-  scale_colour_manual(name="Taxonomic Groups",values= c(taxCols))+ggtitle("")
-ggsave("../Symb.pdf",width=8,height=6)
+# LEGEND
+low$fin <- c("Haptophyte","Haptophyte","Rhizaria","Rhizaria")
+low$x <- 1:4
+low$y <- 2:5
+ggplot(low,aes(x=x,y=y,fill=fin))+geom_point(shape=21,size=3)+scale_fill_manual(name="Taxonomic Groups",values=c(taxCols))+theme_classic()
+ggsave("../../MutualismLegend.pdf")
