517692bc033619a571181ce7c22055363c4ec5d3
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-03-14 15:59:11 -0400

    Update Step4_VizNetworks.R

--- Step4_VizNetworks.R
+++ Step4_VizNetworks.R
@@ -1,64 +1,67 @@
 ### SPOT Network Analysis ###
 ### Visualize Networks + Calculate Network Statistics ###
 ### By: Samantha Gleich ###
-### Last Updated: 2/14/23 ###
+### Last Updated: 3/14/23 ###
 
 taxCols <- c("#E1746D","#76C3D7","#DE5AB1","#D5E0AF","#DED3DC","#87EB58","#D4DC60","#88E5D3","#88AAE1","#DBA85C","#8B7DDA","#9A8D87","#D99CD1","#B649E3","#7EDD90")
 names(taxCols) <- c("Chlorophyte","Ciliate","Cryptophyte","Diatom","Dinoflagellate","Fungi","Haptophyte","MAST","Other Alveolate","Other Archaeplastida","Other Eukaryote","Other Stramenopile","Rhizaria","Syndiniales","Unknown Eukaryote")
 
+s5 <- read.csv(file.choose(),header=TRUE,row.names=1)
+sDCM <- read.csv(file.choose(),header=TRUE,row.names=1)
+colnames(s5) <- str_remove_all(colnames(s5),"^X")
+colnames(sDCM) <- str_remove_all(colnames(sDCM),"^X")
 
-# Load in network outputs
-surf <- read.csv(file.choose(),header=TRUE,row.names=1)
-dcm <- read.csv(file.choose(),header=TRUE,row.names=1)
+s5 <- as.matrix(s5)
+sDCM <- as.matrix(sDCM)
 
-surfName <- colnames(surf)
-surf <- as.matrix(surf)
-gSurf <- graph_from_adjacency_matrix(surf,mode="undirected")
-surfEdge <- get.edgelist(gSurf)
-surfEdge <- as.data.frame(surfEdge)
+s5[s5<0] <- -1
+s5[s5>0] <- 1
+sDCM[sDCM<0] <- -1
+sDCM[sDCM>0] <- 1
 
-dcmName <- colnames(dcm)
-dcm <- as.matrix(dcm)
-gDCM <- graph_from_adjacency_matrix(dcm,mode="undirected")
-dcmEdge <- get.edgelist(gDCM)
-dcmEdge <- as.data.frame(dcmEdge)
 
-# Find edges in surface and DCM network 
-inter <- graph.intersection(gSurf,gDCM)
-interEdge <- get.edgelist(inter)
-interEdge <- as.data.frame(interEdge)
+g5m <- graph_from_adjacency_matrix(s5,mode="undirected",weighted=TRUE)
+gdcm <- graph_from_adjacency_matrix(sDCM,mode="undirected",weighted=TRUE)
 
-# Find edges that are only in surface or only in DCM network 
-`%ni%` <- Negate(`%in%`)
-surfEdge$fin <- paste(surfEdge$V1,surfEdge$V2,sep="_")
-dcmEdge$fin <- paste(dcmEdge$V1,dcmEdge$V2,sep="_")
+int <- graph.intersection(g5m,gdcm)
+
+interEdge <- as.data.frame(get.edgelist(int))
+g5mEdge <- as.data.frame(get.edgelist(g5m))
+g5mEdge$weight5 <- E(g5m)$weight
+gdcmEdge <- as.data.frame(get.edgelist(gdcm))
+gdcmEdge$weightDCM <- E(gdcm)$weight
+
 interEdge$fin1 <- paste(interEdge$V1,interEdge$V2,sep="_")
-interEdge$fin2 <- paste(interEdge$V2,interEdge$V1,sep="_")
+g5mEdge$fin1 <- paste(g5mEdge$V1,g5mEdge$V2,sep="_")
+g5mEdge$fin2 <- paste(g5mEdge$V2,g5mEdge$V1,sep="_")
+gdcmEdge$fin1 <- paste(gdcmEdge$V1,gdcmEdge$V2,sep="_")
+gdcmEdge$fin2 <- paste(gdcmEdge$V2,gdcmEdge$V1,sep="_")
 
-surfEdgeOnly <- subset(surfEdge,fin %ni% interEdge$fin1 & fin %ni% interEdge$fin2)
-dcmEdgeOnly <- subset(dcmEdge,fin %ni% interEdge$fin1 & fin %ni% interEdge$fin2)
+interEdge <- left_join(interEdge,g5mEdge)
+interEdge <- left_join(interEdge,gdcmEdge)
 
-surfEdgeOnly$fin <- NULL
-dcmEdgeOnly$fin <- NULL
-interEdge$fin1 <- NULL
-interEdge$fin2 <- NULL
+interEdge <- subset(interEdge,weight5==weightDCM)
 
-surfEdgeOnly <- as.matrix(surfEdgeOnly)
-dcmEdgeOnly <- as.matrix(dcmEdgeOnly)
-interEdge<- as.matrix(interEdge)
+tax <- read.delim("taxonomy_90.tsv",header=TRUE,row.names=NULL)
+tax$Confidence <- NULL
+colnames(tax) <- c("V1","Tax1")
+interEdge <- left_join(interEdge,tax)
+colnames(tax) <- c("V2","Tax2")
+interEdge <- left_join(interEdge,tax)
 
-gSurfOnly <- graph_from_edgelist(surfEdgeOnly,directed=FALSE)
-gDcmOnly <- graph_from_edgelist(dcmEdgeOnly,directed=FALSE)
-gInterOnly <- graph_from_edgelist(interEdge,directed=FALSE)
+interEdge <- interEdge[c(1:2,4)]
+
+gInterOnly <- graph_from_data_frame(interEdge,directed=FALSE)
+gInterOnly$Weight <- interEdge$weight5
 
 # Visualize networks
-pdf("Surface_Network.pdf")
-plot(gSurfOnly,vertex.size=4,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gSurfOnly),vertex.color="grey",vertex.color="grey20",edge.color="indianred",main="Associations only found at surface (5 m)")
-dev.off()
+# pdf("Surface_Network.pdf")
+# plot(gSurfOnly,vertex.size=4,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gSurfOnly),vertex.color="grey",vertex.color="grey20",edge.color="indianred",main="Associations only found at surface (5 m)")
+# dev.off()
 
-pdf("DCM_Network.pdf")
-plot(gDcmOnly,vertex.size=4,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gDcmOnly),vertex.color="grey",vertex.color="grey20",edge.color="deepskyblue4",main="Associations only found at DCM")
-dev.off()
+# pdf("DCM_Network.pdf")
+# plot(gDcmOnly,vertex.size=4,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gDcmOnly),vertex.color="grey",vertex.color="grey20",edge.color="deepskyblue4",main="Associations only found at DCM")
+# dev.off()
 
 pdf("Intersect_Network.pdf")
 plot(gInterOnly,vertex.size=4,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gInterOnly),vertex.color="grey",vertex.color="grey20",edge.color="olivedrab4",main="Associations found at surface and DCM (merged network)")
@@ -82,11 +85,11 @@ dfDeg <- left_join(dfDeg,t)
 
 # Save edgelists 
 interEdge <- as.data.frame(interEdge)
-surfEdgeOnly <- as.data.frame(surfEdgeOnly)
-dcmEdgeOnly <- as.data.frame(dcmEdgeOnly)
+# surfEdgeOnly <- as.data.frame(surfEdgeOnly)
+# dcmEdgeOnly <- as.data.frame(dcmEdgeOnly)
 write.csv(interEdge,"Surf_DCM_edges.csv")
-write.csv(surfEdgeOnly,"Surf_edges.csv")
-write.csv(dcmEdgeOnly,"DCM_edges.csv")
+# write.csv(surfEdgeOnly,"Surf_edges.csv")
+# write.csv(dcmEdgeOnly,"DCM_edges.csv")
 
 
 # Size by degree
@@ -127,20 +130,44 @@ plot(gInterOnly,vertex.size=V(gInterOnly)$deg*0.5,vertex.label=NA,edge.width=0.5
 dev.off()
 
 counts <- read.delim("feature-table.tsv",header=TRUE,row.names=1)
+manifest <- read.delim("manifest.txt",header=FALSE,sep=",")
+
+# We need to rename our samples in the counts dataframe so that they contain meaningful information
+namez <- colsplit(manifest$V2,"-",c("a","b","c","d","e","f","g"))
+namez$depth <- ifelse(grepl("5m",namez$g),"5m","DCM")
+# Making a "fin" column that contains the date and depth of each sample
+namez$fin <- paste(namez$c,namez$d,namez$e,namez$f,namez$depth,sep="_")
+
+# Make a new dataframe with old sample name info and new sample name info
+fin <- data.frame(namez=c(as.character(manifest$V1)),new=c(namez$fin))
+fin$new <- paste("SPOT",fin$new,sep="_")
+fin <- fin %>% distinct(new,.keep_all = TRUE)
+
+# Let's add the new (meaningful) sample names to our counts dataframe 
 counts <- as.data.frame(t(counts))
-counts <- counts/rowSums(counts)
-counts <- as.data.frame(t(counts))
-counts <- subset(counts,rownames(counts) %in% dfDeg$name)
-counts$meanz <- rowMeans(counts)
-counts$median = apply(counts, 1, median, na.rm=T)
-c <- data.frame(name=c(rownames(counts)),meanz=c(counts$meanz))
-#c <- data.frame(name=c(rownames(counts)),median=c(counts$median))
+counts$namez <- rownames(counts)
+dfNew <- left_join(counts,fin)
+rownames(dfNew) <- dfNew$new
+dfNew$namez <- NULL
+dfNew$new <- NULL
+dfNew<- subset(dfNew,rownames(dfNew)!="SPOT_115_2_16_12_5m"& rownames(dfNew)!="SPOT_115_2_16_12_DCM")
+dfNew <- as.data.frame(clr(dfNew))
+
+dfNew <- as.data.frame(t(dfNew))
+dfNew <- subset(dfNew,rownames(dfNew) %in% V(gInterOnly)$name)
+dfNew$sumz <- rowMeans(dfNew)
+c <- data.frame(name=c(rownames(dfNew)),mean=c(dfNew$sumz))
 dfDeg <- left_join(dfDeg,c)
-V(gInterOnly)$ab2 <- dfDeg$meanz
+V(gInterOnly)$ab <- (dfDeg$mean)+0.5
+
+
 
 pdf("../../Abundance3.pdf")
-plot(gInterOnly,vertex.size=V(gInterOnly)$ab2*800,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gInterOnly),vertex.color=V(gInterOnly)$col,edge.color="grey30",main="")
+plot(gInterOnly,vertex.size=V(gInterOnly)$ab*4,vertex.label=NA,edge.width=0.5,layout=layout_with_kk(gInterOnly),vertex.color=V(gInterOnly)$col,edge.color="grey30",main="")
 dev.off()
 
-ggplot(dfDeg,aes(x=deg,y=meanz,fill=tax))+geom_point(shape=21,size=2)+scale_fill_manual(name=c("Taxonomic Group"),values=c(taxCols))+theme_classic(base_size = 14)+xlab("Number of Associations")+ylab("Relative Abundance")+ylim(0,0.06)+theme(legend.position='top')
-ggsave("../../plot2.pdf",width=8,height=4)
+ggplot(dfDeg,aes(x=deg,y=mean,fill=tax))+geom_point(shape=21,size=2)+scale_fill_manual(name=c("Taxonomic Group"),values=c(taxCols))+theme_classic(base_size = 14)+xlab("Number of Associations")+ylab("CLR-transformed Abundance")+theme(legend.position='top')
+ggsave("../../plot1.pdf",width=5,height=5)
+
+linMod <- lm(dfDeg$mean~dfDeg$deg)
+summary(linMod)
