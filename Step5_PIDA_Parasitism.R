6865d4c1ae6ba7687e7fbddc2829c1f3da9db082
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-04-05 10:48:54 -0400

    Update Step5_PIDA_Parasitism.R

--- Step5_PIDA_Parasitism.R
+++ Step5_PIDA_Parasitism.R
@@ -1,51 +1,19 @@
 ### SPOT Network Analysis ###
-### PIDA Comparison ###
+### PIDA Comparison - PARASITISM ###
 ### By: Samantha Gleich ###
-### Last Updated: 3/1/23 ###
+### Last Updated: 4/5/23 ###
 
 # Colors to use for all taxonomic analyses
 taxCols <- c("#E1746D","#76C3D7","#DE5AB1","#D5E0AF","#DED3DC","#87EB58","#D4DC60","#88E5D3","#88AAE1","#DBA85C","#8B7DDA","#9A8D87","#D99CD1","#B649E3","#7EDD90")
 names(taxCols) <- c("Chlorophyte","Ciliate","Cryptophyte","Diatom","Dinoflagellate","Fungi","Haptophyte","MAST","Other Alveolate","Other Archaeplastida","Other Eukaryote","Other Stramenopile","Rhizaria","Syndiniales","Unknown Eukaryote")
 
-s5 <- read.csv(file.choose(),header=TRUE,row.names=1)
-sDCM <- read.csv(file.choose(),header=TRUE,row.names=1)
-colnames(s5) <- str_remove_all(colnames(s5),"^X")
-colnames(sDCM) <- str_remove_all(colnames(sDCM),"^X")
-
-s5 <- as.matrix(s5)
-sDCM <- as.matrix(sDCM)
-
-s5[s5<0] <- -1
-s5[s5>0] <- 1
-sDCM[sDCM<0] <- -1
-sDCM[sDCM>0] <- 1
-
-
-g5m <- graph_from_adjacency_matrix(s5,mode="undirected",weighted=TRUE)
-gdcm <- graph_from_adjacency_matrix(sDCM,mode="undirected",weighted=TRUE)
-
-int <- graph.intersection(g5m,gdcm)
-
-interEdge <- as.data.frame(get.edgelist(int))
-g5mEdge <- as.data.frame(get.edgelist(g5m))
-g5mEdge$weight5 <- E(g5m)$weight
-gdcmEdge <- as.data.frame(get.edgelist(gdcm))
-gdcmEdge$weightDCM <- E(gdcm)$weight
-
-interEdge$fin1 <- paste(interEdge$V1,interEdge$V2,sep="_")
-g5mEdge$fin1 <- paste(g5mEdge$V1,g5mEdge$V2,sep="_")
-g5mEdge$fin2 <- paste(g5mEdge$V2,g5mEdge$V1,sep="_")
-gdcmEdge$fin1 <- paste(gdcmEdge$V1,gdcmEdge$V2,sep="_")
-gdcmEdge$fin2 <- paste(gdcmEdge$V2,gdcmEdge$V1,sep="_")
-
-interEdge <- left_join(interEdge,g5mEdge)
-interEdge <- left_join(interEdge,gdcmEdge)
-
-interEdge <- subset(interEdge,weight5==weightDCM)
+interEdge <- read.csv(file.choose(),header=TRUE)
 
 tax <- read.delim("taxonomy_90.tsv",header=TRUE,row.names=NULL)
 tax$Confidence <- NULL
 colnames(tax) <- c("V1","Tax1")
+interEdge$V1 <- str_remove_all(interEdge$V1,"S_")
+interEdge$V2 <- str_remove_all(interEdge$V2,"S_")
 interEdge <- left_join(interEdge,tax)
 colnames(tax) <- c("V2","Tax2")
 interEdge <- left_join(interEdge,tax)
@@ -77,13 +45,8 @@ for (i in 1:length(genera)){
     keeperz <- c(keeperz,g)
   }
 }
-keeperz <- keeperz[-c(4,12)]
 
 
-cilSyn <- keeperz[c(1:2)]
-dinoSyn <- keeperz[c(3:10)]
-
-interEdge <- interEdge[c(1:2,4,7:8)]
 synG1 <- subset(interEdge,grepl("Group-I-",interEdge$Tax1)|grepl("Group-I-",interEdge$Tax2))
 synG2 <- subset(interEdge,grepl("Group-II-",interEdge$Tax1)|grepl("Group-II-",interEdge$Tax2))
 cerc<- subset(interEdge,grepl("Cercoz",interEdge$Tax1)|grepl("Cercoz",interEdge$Tax2))
@@ -91,6 +54,7 @@ cerc<- subset(interEdge,grepl("Cercoz",interEdge$Tax1)|grepl("Cercoz",interEdge$
 # Group I Syndiniales (Infect ciliates)
 Strob <- subset(synG1,grepl("Strombidium",synG1$Tax1)|grepl("Strombidium",synG1$Tax2))
 Pelago <- subset(synG1,grepl("Pelagostrobilidium",synG1$Tax1)|grepl("Pelagostrobilidium",synG1$Tax2))
+Eut <- subset(synG1,grepl("Eutintinnus",synG1$Tax1)|grepl("Eutintinnus",synG1$Tax2))
 
 # Group II Syndiniales (Infect Dinoflagellate)
 Proro <- subset(synG2,grepl("Prorocentrum",synG2$Tax1)|grepl("Prorocentrum",synG2$Tax2))
@@ -101,16 +65,20 @@ Hetero<- subset(synG2,grepl("Heterocapsa",synG2$Tax1)|grepl("Heterocapsa",synG2$
 Gymno<- subset(synG2,grepl("Gymnodinium",synG2$Tax1)|grepl("Gymnodinium",synG2$Tax2))
 Karlo<- subset(synG2,grepl("Karlodinium",synG2$Tax1)|grepl("Karlodinium",synG2$Tax2))
 Lepid<- subset(synG2,grepl("Lepidodinium",synG2$Tax1)|grepl("Lepidodinium",synG2$Tax2))
+Alex<- subset(synG2,grepl("Alexandrium",synG2$Tax1)|grepl("Alexandrium",synG2$Tax2))
+Proto<- subset(synG2,grepl("Protoperidinium",synG2$Tax1)|grepl("Protoperidinium",synG2$Tax2))
 
 # Cercozoa no matches to either diatom genus
+Cerc<- subset(interEdge,grepl("Cryothe",interEdge$Tax1)|grepl("Cryotheco",interEdge$Tax2))
+Cerc<- subset(Cerc,grepl("Bacillari",Cerc$Tax1)|grepl("Bacillari",Cerc$Tax2))
 
-full <- rbind(Strob,Pelago,Proro,Gyro,Gony,Trip,Hetero,Gymno,Karlo,Lepid)
-full <- full[c(1:3)]
+full <- rbind(Strob,Pelago,Eut,Proro,Gyro,Gony,Trip,Hetero,Gymno,Karlo,Lepid,Alex,Proto,Cerc)
+full <- full[c(2:4)]
 
 
 # Plot 
 outG <-graph_from_data_frame(full,directed=FALSE)
-E(outG)$weight <- full$weight5
+E(outG)$weight <- full$weight
 outDf <- data.frame(name=c(V(outG)$name))
 colnames(outDf) <- "V2"
 outDf <- left_join(outDf,tax)
@@ -133,6 +101,7 @@ taxz$fin <- ifelse(is.na(taxz$fin),"Other Eukaryote",taxz$fin)
 
 V(outG)$fin <- taxz$fin
 taxz$Low <- paste(taxz$Genus, taxz$Species,sep="_")
+taxz$Low <- ifelse(taxz$Low=="_",taxz$Family,taxz$Low)
 low <- colsplit(taxz$Low,"_",c("keep","keep2","toss"))
 V(outG)$namez <- low$keep
 
@@ -184,21 +153,22 @@ taxz$fin <- ifelse(is.na(taxz$fin),"Other Eukaryote",taxz$fin)
 
 V(finalG)$fin <- taxz$fin
 taxz$Low <- paste(taxz$Genus, taxz$Species,sep="_")
+taxz$Low <- ifelse(taxz$Low=="_",taxz$Family,taxz$Low)
 low <- colsplit(taxz$Low,"_",c("keep","keep2","toss"))
 V(finalG)$namez <- low$keep
 
 # PLOT
-pdf("../../try.pdf",width=20,height=11)
+pdf("../../PARASITISM_APRIL5.pdf",width=20,height=15)
 ggraph(finalG, layout = 'linear', circular = TRUE) + geom_edge_arc(aes(color = weight),alpha=0.6,width=1) + 
-  geom_node_text(aes(label = namez, angle = node_angle(x, y)), hjust = -0.1,size=3) +
-  geom_node_point(shape = 21, size = 4, aes(fill = fin)) +
+  geom_node_text(aes(label = namez, angle = node_angle(x, y)), hjust = -0.15,size=3) +
+  geom_node_point(shape = 21, size = 6, aes(fill = fin)) +
   theme_graph() +
-  scale_edge_color_gradientn(colours = c('indianred', 'white', 'dodgerblue')) +
+  scale_edge_color_gradientn(colours = c('indianred', 'white', 'dodgerblue'),values=c(-1,0,1)) +
   coord_fixed(xlim = c(-1.4, 1.4), ylim = c(-1.4, 1.4)) +scale_fill_manual(name="Taxonomic Groups",values=c(taxCols))
 dev.off()
 
 # LEGEND
-outG3$x <- 1:41
-outG3$y <- 2:42
+outG3$x <- 1:21
+outG3$y <- 2:22
 ggplot(outG3,aes(x=x,y=y,fill=fin2))+geom_point(shape=21,size=3)+scale_fill_manual(name="Taxonomic Groups",values=c(taxCols))+theme_classic()
 ggsave("../../ParasitismLegend.pdf")
