36235e9811a7d3c4378af4e8a914393302717724
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-05-15 12:19:02 -0400

    Update Step1b_RDA.R

--- Step1b_RDA.R
+++ Step1b_RDA.R
@@ -1,7 +1,7 @@
 ### SPOT Network Analysis ###
-### RDA ###
+### Figure 3: RDA ###
 ### By: Samantha Gleich ###
-### Last Updated: 4/3/23 ###
+### Last Updated: 5/12/23 ###
 
 # Load libraries
 library(tidyverse)
@@ -10,18 +10,19 @@ library(ggplot2)
 library(randomcoloR)
 library(vegan)
 library(compositions)
+library(missForest)
 
-# Colors to use for all taxonomic analyses
-taxCols <- c("#E1746D","#76C3D7","#DE5AB1","#D5E0AF","#DED3DC","#87EB58","#D4DC60","#88E5D3","#88AAE1","#DBA85C","#8B7DDA","#9A8D87","#D99CD1","#B649E3","#7EDD90")
-names(taxCols) <- c("Chlorophyte","Ciliate","Cryptophyte","Diatom","Dinoflagellate","Fungi","Haptophyte","MAST","Other Alveolate","Other Archaeplastida","Other Eukaryote","Other Stramenopile","Rhizaria","Syndiniales","Unknown Eukaryote")
+# Set SPOT working directory
+setwd("~/Desktop/SPOT/SPOT_2023")
 
 # Load in counts data and manifest file
-counts <- read.delim(file.choose(),header=TRUE,row.names=1)
-manifest <- read.delim(file.choose(),header=FALSE,sep=",")
+counts <- read.delim("feature-table.tsv",header=TRUE,row.names=1)
+manifest <- read.delim("manifest.txt",header=FALSE,sep=",")
 
 # We need to rename our samples in the counts dataframe so that they contain meaningful information
 namez <- colsplit(manifest$V2,"-",c("a","b","c","d","e","f","g"))
 namez$depth <- ifelse(grepl("5m",namez$g),"5m","DCM")
+
 # Making a "fin" column that contains the date and depth of each sample
 namez$fin <- paste(namez$c,namez$d,namez$e,namez$f,namez$depth,sep="_")
 
@@ -39,7 +40,7 @@ dfNew$namez <- NULL
 dfNew$new <- NULL
 
 # Add taxonomy information
-tax <- read.delim(file.choose(),header=TRUE)
+tax <- read.delim("taxonomy_90.tsv",header=TRUE)
 tax <- tax[c(1:2)]
 dfNew <- as.data.frame(t(dfNew))
 dfNew$Feature.ID <- rownames(dfNew)
@@ -49,61 +50,70 @@ dfTax$Feature.ID <- NULL
 dfTax$Taxon <- NULL
 dfTax <- as.data.frame(t(dfTax))
 
+runRDA <- function(df,depth){
+  df <- subset(df,grepl(paste(depth),rownames(df)))
+  df <- subset(df,rownames(df)!="SPOT_115_2_16_12_5m"& rownames(df)!="SPOT_115_2_16_12_DCM")
+  
+  normDf <- as.data.frame(clr(df))
+  env <- read.csv("SPOT_Env_NewJan11.csv",header=TRUE)
+  colz <- colsplit(rownames(normDf),"_",c("spot","Cruise","month","day","year","Depth"))
+  colz <- left_join(colz,env)
+  
+  if (depth=="5m"){
+    keepEnv <- c("DayOfYear","O2Wink","NH4","SiO3","PO4","NO2.NO3","CTDTMP","CTDBEAM","CTDFLUOR","CTDOXY","CTDSAL","DayLength","DayDiff","SLA","cyanos","sar11","cyanos_PA","sar11_PA","MEI","SST","Chla","PP")}
+  
+  if (depth=="DCM"){
+    keepEnv <- c("DayOfYear","CSDepth","O2Wink","NH4","SiO3","PO4","NO2.NO3","CTDTMP","CTDBEAM","CTDFLUOR","CTDOXY","CTDSAL","DayLength","DayDiff","SLA","cyanos","sar11","cyanos_PA","sar11_PA","MEI","SST","Chla","PP")}
+  
+  envTable <- subset(colz,select=c(keepEnv))
+  set.seed(100)
+  envTable <- missForest::missForest(envTable)
+  envTable <- data.frame(envTable$ximp)
+  
+  envTable <- decostand(envTable, method = "standardize")
+  
+  tryAgain=TRUE
+  while(tryAgain==TRUE){
+    rdaOut<- rda(normDf ~ ., data = envTable)
+    rdaDf <- data.frame(rdaOut$CCA$u)
+    rdaDf$Month <- colz$Month
+    
+    finalmodel<- ordistep(rdaOut, scope=formula(rdaOut))
+    t <- vif.cca(finalmodel)
+    t2 <- which(t>=10)
+    tryAgain <- ifelse(length(t2)>=1,TRUE,FALSE)
+    
+    if(length(t2)>=1){
+      t <- which.max(t)}
+  
+  envTable <- envTable[,!names(envTable) %in% c(names(t))]}
+  summar <- summary(rdaOut)
+  summar <- data.frame(summar$cont$importance)
+  summar <- subset(summar,select=c(grepl("RDA",colnames(summar))))
+  anovOut <- anova.cca(finalmodel, by="terms")
+  anovOutSub <- subset(anovOut,`Pr(>F)` < 0.01)
+  keep <- rownames(anovOutSub)
+  
+  fitDf <- data.frame(rdaOut$CCA$biplot)
+  fitDf <- fitDf[c(1:2)]
+  fitDf$x <- mean(rdaDf$RDA1)
+  fitDf$y <- mean(rdaDf$RDA2)
+  
+  fitDf <- subset(fitDf,rownames(fitDf) %in% keep)
+  return(list(rdaDf,fitDf,summar))}
+  
+  outSurf <- runRDA(dfTax,"5m")
+  mainDf <- outSurf[[1]]
+  fitDf <- outSurf[[2]]
+  statDf <- outSurf[[3]]
+  
+  outDCM <- runRDA(dfTax,"DCM")
+  mainDf2 <- outDCM[[1]]
+  fitDf2 <- outDCM[[2]]
+  statDf2 <- outDCM[[3]]
+  
 
-dfTax <- subset(dfTax,grepl("DCM",rownames(dfTax)))
-dfTax <- subset(dfTax,rownames(dfTax)!="SPOT_115_2_16_12_5m"& rownames(dfTax)!="SPOT_115_2_16_12_DCM")
-# Make NMDS plot with bray-curtis dissimilarity
-# normDf <- decostand(dfTax,method="rclr")
-normDf <- as.data.frame(clr(dfTax))
-# normDf <- clr(dfTax)
-# normDf <- as.data.frame(normDf)
-env <- read.csv(file.choose(),header=TRUE)
-colz <- colsplit(rownames(normDf),"_",c("spot","Cruise","month","day","year","Depth"))
-colz <- left_join(colz,env)
-mytable <- colz[c(10:32)]
-# mytable$CSDepth <- NULL
-set.seed(100)
-mytable <- missForest::missForest(mytable)
-mytable <- mytable$ximp
 
-# Remove for surface
-mytable$SST <- NULL
-mytable$DayDiff <- NULL
-mytable$CTDTMP <- NULL
-
-# Remove for DCM
-mytable$DayDiff <- NULL
-mytable$SST <- NULL
-
-mytable <- decostand(mytable, method = "standardize")
-
-# decorana (normDf)
-out<- rda(normDf ~ ., data = mytable)
-rdaDf <- data.frame(out$CCA$u)
-
-
-rdaDf$Month <- colz$Month
-# rdaDf[is.na(rdaDf)] <- 2
-
-finalmodel<- ordistep(out, scope=formula(out))
-vif.cca(finalmodel)
-#anova(out, permutations=9999)
-anovOut <- anova.cca(finalmodel, by="terms")
-anovOutSub <- subset(anovOut,`Pr(>F)` < 0.01)
-keep <- rownames(anovOutSub)
-
-fitDf <- data.frame(out$CCA$biplot)
-fitDf <- fitDf[c(1:2)]
-fitDf$x <- mean(rdaDf$RDA1)
-fitDf$y <- mean(rdaDf$RDA2)
-
-# t <- envfit(out,mytable)
-# t <- data.frame(t$vectors$arrows)
-# fitDf$RDA1 <- t$RDA1
-# fitDf$RDA2 <- t$RDA2
-
-fitDf <- subset(fitDf,rownames(fitDf) %in% keep)
-
-outP <- ggplot(rdaDf,aes(RDA1,RDA2,fill=factor(Month),shape=as.factor(Month)))+geom_point(size=2,color="black")+scale_fill_manual(name="Month",values=c("dodgerblue","dodgerblue","darkolivegreen4","darkolivegreen4","darkolivegreen4","goldenrod1","goldenrod1","goldenrod1","firebrick2","firebrick2","firebrick2","dodgerblue"),labels=c("January","February","March","April","May","June","July","August","September","October","November","December"))+scale_shape_manual(name="Month",values=c(21,22,21,22,24,21,22,24,21,22,24,24),labels=c("January","February","March","April","May","June","July","August","September","October","November","December"))+theme_classic()+xlab("RDA1 (3.63%)")+ylab("RDA2 (2.06%)")+geom_vline(xintercept = 0,linetype="dotted")+geom_hline(yintercept = 0,linetype="dotted")+ggtitle("DCM")+geom_segment(aes(x = fitDf[1,3], y = fitDf[1,4], xend = fitDf[1,1] , yend = fitDf[1,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[2,3], y = fitDf[2,4], xend = fitDf[2,1] , yend = fitDf[2,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[3,3], y = fitDf[3,4], xend = fitDf[3,1] , yend = fitDf[3,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[4,3], y = fitDf[4,4], xend = fitDf[4,1] , yend = fitDf[4,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[5,3], y = fitDf[5,4], xend = fitDf[5,1] , yend = fitDf[5,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[6,3], y = fitDf[6,4], xend = fitDf[6,1] , yend = fitDf[6,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[7,3], y = fitDf[7,4], xend = fitDf[7,1] , yend = fitDf[7,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[8,3], y = fitDf[8,4], xend = fitDf[8,1] , yend = fitDf[8,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)
+outP <- ggplot(mainDf2,aes(RDA1,RDA2,fill=factor(Month),shape=as.factor(Month)))+geom_point(size=2,color="black")+scale_fill_manual(name="Month",values=c("dodgerblue","dodgerblue","darkolivegreen4","darkolivegreen4","darkolivegreen4","goldenrod1","goldenrod1","goldenrod1","firebrick2","firebrick2","firebrick2","dodgerblue"),labels=c("January","February","March","April","May","June","July","August","September","October","November","December"))+scale_shape_manual(name="Month",values=c(21,22,21,22,24,21,22,24,21,22,24,24),labels=c("January","February","March","April","May","June","July","August","September","October","November","December"))+theme_classic()+xlab("RDA1 (3.63%)")+ylab("RDA2 (2.06%)")+geom_vline(xintercept = 0,linetype="dotted")+geom_hline(yintercept = 0,linetype="dotted")+ggtitle("DCM")+geom_segment(aes(x = fitDf[1,3], y = fitDf[1,4], xend = fitDf[1,1] , yend = fitDf[1,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[2,3], y = fitDf[2,4], xend = fitDf[2,1] , yend = fitDf[2,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[3,3], y = fitDf[3,4], xend = fitDf[3,1] , yend = fitDf[3,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[4,3], y = fitDf[4,4], xend = fitDf[4,1] , yend = fitDf[4,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[5,3], y = fitDf[5,4], xend = fitDf[5,1] , yend = fitDf[5,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[6,3], y = fitDf[6,4], xend = fitDf[6,1] , yend = fitDf[6,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[7,3], y = fitDf[7,4], xend = fitDf[7,1] , yend = fitDf[7,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)+geom_segment(aes(x = fitDf[8,3], y = fitDf[8,4], xend = fitDf[8,1] , yend = fitDf[8,2]),arrow = arrow(length=unit(3, "mm")),linewidth=0.5)
 outP
-ggsave("../../DCM_April2023.pdf",width=6,height=4)
+# ggsave("DCMRDA_May2023.pdf",width=6,height=4)
