3256dd99c9ebbdeb0f271758bfb31d5af99389f4
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2024-02-06 17:44:45 -0500

    Update Figure6_Predation.R

--- Figure6_Predation.R
+++ Figure6_Predation.R
@@ -1,7 +1,7 @@
-  ### SPOT Network Analysis ###
+### SPOT Network Analysis ###
 ### Figure 6: PIDA Comparison - PREDATION ###
 ### By: Samantha Gleich ###
-### Last Updated: 12/5/23 ###
+### Last Updated: 2/6/24 ###
 
 # Load libraries
 library(igraph)
@@ -13,8 +13,8 @@ library(stringr)
 library(stringi)
 
 # Find edges that are in surface and DCM networks
-surf <- read.csv("../../Surf_SPOT_Dec2023.csv",header=TRUE,row.names=1)
-dcm <- read.csv("../../DCM_SPOT_Dec2023.csv",header=TRUE,row.names=1)
+surf <- read.csv("Surf_SPOT_Feb2024.csv",header=TRUE,row.names=1)
+dcm <- read.csv("DCM_SPOT_Feb2024.csv",header=TRUE,row.names=1)
 
 namez <- rownames(surf)
 namez <- str_remove_all(namez,"S_")
@@ -34,21 +34,23 @@ intG <- graph.intersection(surfG,dcmG)
 interEdge <- data.frame(get.edgelist(intG))
 
 # Surface graph
-surfEdge <- data.frame(get.edgelist(surfG))
-tax <- read.delim("taxonomy_90.tsv",header=TRUE,row.names=NULL)
+surfEdge <- data.frame(get.edgelist(dcmG))
+tax <- read.delim("taxonomy.tsv",header=TRUE,row.names=NULL)
 tax$Confidence <- NULL
 colnames(tax) <- c("X1","Tax1")
 surfEdge <- left_join(surfEdge,tax)
 colnames(tax) <- c("X2","Tax2")
 surfEdge <- left_join(surfEdge,tax)
 
+surfEdge <-subset(surfEdge,grepl("Eukaryota",surfEdge$Tax1)|grepl("Eukaryota",surfEdge$Tax2))
+
 # Load in PIDA
 pida <- read.csv("PIDA_Int.csv",header=TRUE) 
 pidaPred <- subset(pida,Taxonomic.interaction=="Prot - Prot" & Interaction=="pred")
 
 # Parasitism edges
-surf1 <- colsplit(surfEdge$Tax1,";",c("super1","k1","p1","c1","o1","f1","g1","s1"))
-surf2 <- colsplit(surfEdge$Tax2,";",c("super2","k2","p2","c2","o2","f2","g2","s2"))
+surf1 <- colsplit(surfEdge$Tax1,";",c("super1","d1","k1","p1","c1","o1","f1","g1","s1"))
+surf2 <- colsplit(surfEdge$Tax2,";",c("super2","d2","k2","p2","c2","o2","f2","g2","s2"))
 surfEdge <- cbind(surfEdge,surf1,surf2)
 
 # Find all unique genera in surface network
@@ -98,22 +100,23 @@ colnames(outDf) <- "X2"
 outDf <- left_join(outDf,tax)
 
 # Assign each ASV to taxonomic group for color coding
-taxz <- colsplit(outDf$Tax2,";",c("Supergroup","Kingdom","Phylum","Class","Order","Family","Genus","Species"))
+taxz <- colsplit(outDf$Tax2,";",c("Supergroup","Domain","Kingdom","Phylum","Class","Order","Family","Genus","Species"))
 taxz$fin <- ifelse(taxz$Class=="Dinophyceae","Dinoflagellate",NA)
 taxz$fin <- ifelse(taxz$Order=="Dino-Group-I","Group I Syndiniales",taxz$fin)
 taxz$fin <- ifelse(taxz$Order=="Dino-Group-II","Group II Syndiniales",taxz$fin)
-taxz$fin <- ifelse(taxz$Class=="Bacillariophyta","Diatom",taxz$fin)
-taxz$fin <- ifelse(grepl("MAST",taxz$Class),"MAST",taxz$fin)
+taxz$fin <- ifelse(taxz$Class=="Mediophyceae"|taxz$Class=="Coscinodiscophyceae"|taxz$Class=="Bacillariophyceae","Diatom",taxz$fin)
+taxz$fin <- ifelse(grepl("MAST",taxz$Family),"MAST",taxz$fin)
 taxz$fin <- ifelse(taxz$Kingdom=="Rhizaria","Rhizaria",taxz$fin)
-taxz$fin <- ifelse(taxz$Phylum=="Chlorophyta","Chlorophyte",taxz$fin)
-taxz$fin <- ifelse(taxz$Phylum=="Cryptophyta","Cryptophyte",taxz$fin)
-taxz$fin <- ifelse(taxz$Phylum=="Haptophyta","Haptophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Chlorophyta","Chlorophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Cryptophyta","Cryptophyte",taxz$fin)
+taxz$fin <- ifelse(taxz$Kingdom=="Haptophyta","Haptophyte",taxz$fin)
 taxz$fin <- ifelse(taxz$Phylum=="Ciliophora","Ciliate",taxz$fin)
 taxz$fin <- ifelse(taxz$Phylum=="Metazoa","Metazoa",taxz$fin)
 taxz$fin <- ifelse(taxz$Kingdom=="Stramenopiles" & is.na(taxz$fin),"Other Stramenopiles",taxz$fin)
-taxz$fin <- ifelse(taxz$Kingdom=="Archaeplastida" & is.na(taxz$fin),"Other Archaeplastids",taxz$fin)
+taxz$fin <- ifelse(taxz$Domain=="Archaeplastida" & is.na(taxz$fin),"Other Archaeplastids",taxz$fin)
 taxz$fin <- ifelse(taxz$Kingdom=="Alveolata" & is.na(taxz$fin),"Other Alveolate",taxz$fin)
 taxz$fin <- ifelse(is.na(taxz$fin),"Other Eukaryote",taxz$fin)
+unique(taxz$fin)
 
 # Retain lowest taxonomic assignment of each ASV
 outDf$namez <- taxz$fin
@@ -138,22 +141,12 @@ E(outG)$weight[E(outG)[c3]] <- -1
 taxCols <- c("#E1746D","#76C3D7","#DE5AB1","#D5E0AF","#DED3DC","#87EB58","#D4DC60","#88E5D3","#88AAE1","#DBA85C","#8B7DDA","#9A8D87","#D99CD1","#B649E3","#7EDD90")
 names(taxCols) <- c("Chlorophyte","Ciliate","Cryptophyte","Diatom","Haptophyte","Dinoflagellate","MAST","Other Alveolate","Other Archaeplastida","Other Eukaryote","Other Stramenopile","Rhizaria","Group I Syndiniales","Group II Syndiniales","Unknown Eukaryote")
 
-namezTry <- unique(V(outG)$namez)
-namezTry <- as.data.frame(namezTry)
-namezTry$letter <-LETTERS[1:nrow(namezTry)]
-namezTry$letter <- ifelse(namezTry$namezTry=="","",namezTry$letter)
-#namezTry[17,] <- c("Chaetoceros","Q")
-#namezTry[18,] <- c("Leptocylindrus","R")
-#namezTry[19,] <- c("Nitzschia","S")
+d <- data.frame(Name=c(V(outG)$namez))
+namez <- read.csv("PidaPredLetters.csv",header=TRUE)
+d <- left_join(d,namez)
+V(outG)$namez <- d$Letter
 
-for (i in 1:length(V(outG)$namez)){
-  row <- which(namezTry$namezTry==V(outG)$namez[i])
-  V(outG)$namez[i] <- namezTry$letter[row]
-}
-V(outG)$namez
-
-
-pdf("../../Surf_Pred.pdf",width=6,height=4)
+pdf("../DCM_Pred.pdf",width=9,height=6)
 ggraph(outG, layout = 'linear', circular = TRUE) + geom_edge_arc(aes(color = as.factor(weight)),alpha=0.95,width=0.7) +
   geom_node_point(shape = 21, size = 6, aes(fill = fin)) +
   theme_graph() +scale_fill_manual(name="Taxonomic Groups",values=c(taxCols))+scale_edge_color_manual(values=c("grey60","red"),breaks=c(1,-1))+geom_node_text(aes(label = namez),size=4,fontface="bold")
@@ -161,5 +154,5 @@ dev.off()
 
 
 # Legend
-ggplot(taxz,aes(x=1:38,y=2:39,fill=fin))+geom_point(size=6,shape=21)+scale_fill_manual(name="Taxonomic Group",values=c(taxCols))+theme_classic()+theme(legend.position="bottom")
-ggsave("../../Legend.pdf",width=6,height=4)
+ggplot(taxz,aes(x=1:81,y=2:82,fill=fin))+geom_point(size=6,shape=21)+scale_fill_manual(name="Taxonomic Group",values=c(taxCols))+theme_classic()+theme(legend.position="bottom")
+ggsave("../Legend.pdf",width=6,height=4)
